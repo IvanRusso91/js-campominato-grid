828adaa219bfcd2a1505e80dc5e8a0197b6cc206
IvanRusso91 <iv.russo91@gmail.com>
2022-04-21 19:41:29 +0200

    agiunti optioni al js

--- js/script.js
+++ js/script.js
@@ -10,13 +10,21 @@ Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 //-------------------------------------------------------
 
 const field = document.querySelector('.field');
+const userChoice = document.getElementById('choice').value;
 init(field);
+// initSec(field);
+// initTerz(field);
 
 
+if(userChoice === 'easy'){
+  document.getElementById('play','easy').addEventListener('click', initTerz);
+}else if (userChoice === 'medium'){
+  document.getElementById('play','medium').addEventListener('click', initSec);
+}else if(userChoice === 'hard'){
+  document.getElementById('play','hard').addEventListener('click', init);
+}
 
-// document.getElementById('paly').addEventListener('clik' function(){
 
-// })
 
 function init(){
    for(let i = 1; i <= 100 ; i++){
@@ -27,10 +35,47 @@ function init(){
   }
 }
 
- function createSquare(cella, number){
-   const sq = document.createElement('div');
-   sq.className = 'square-100';
-   sq.innerHTML = `<span>${number}</span>`
-   cella.append(sq);
-   return sq;
+
+function initSec(){
+  for(let i = 1; i <= 81 ; i++){
+   const sq = createSquareSeconda(field, i)
+   sq.addEventListener('click', function(){
+     this.classList.add('clicked','even');
+   })
  }
+}
+
+
+function initTerz(){
+  for(let i = 1; i <= 49 ; i++){
+   const sq = createSquareTerza(field, i)
+   sq.addEventListener('click', function(){
+     this.classList.add('clicked','even');
+   })
+ }
+}
+
+
+ function createSquare(cella, number){
+    const sq = document.createElement('div');
+    sq.className = 'square-100';
+    sq.innerHTML = `<span>${number}</span>`
+    cella.append(sq);
+    return sq;
+ }
+
+ function createSquareSeconda(cella, number){
+  const sq = document.createElement('div');
+  sq.className = 'square-81';
+  sq.innerHTML = `<span>${number}</span>`
+  cella.append(sq);
+  return sq;
+}
+
+function createSquareTerza(cella, number){
+  const sq = document.createElement('div');
+  sq.className = 'square-49';
+  sq.innerHTML = `<span>${number}</span>`
+  cella.append(sq);
+  return sq;
+}
